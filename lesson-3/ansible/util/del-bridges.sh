68208a02475c25b6dbbba69542ced5a84e187e2c
brmcdoug <brmcdoug@cisco.com>
2024-06-18 23:13:00 -0700

    external world

--- lesson-3/ansible/util/del-bridges.sh
+++ lesson-3/ansible/util/del-bridges.sh
@@ -1,8 +1,12 @@
 #/bin/bash
 
-sudo ifconfig frr-2-host down
-sudo ifconfig frr-3-host down
+sudo ifconfig frr-1-xrd-1 down
+sudo ifconfig frr-2-xrd-1 down
+sudo ifconfig frr-2-external down
+sudo ifconfig frr-3-external down
 
-sudo brctl delbr frr-2-host
-sudo brctl delbr frr-3-host
+sudo brctl delbr frr-1-xrd-1
+sudo brctl delbr frr-2-xrd-1
+sudo brctl delbr frr-2-external
+sudo brctl delbr frr-3-external
 
