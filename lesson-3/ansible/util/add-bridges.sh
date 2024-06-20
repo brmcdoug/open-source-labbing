6643a8059a621ec81c70c9aaea05e2c4476cfaf0
brmcdoug <brmcdoug@cisco.com>
2024-06-20 11:52:19 -0700

    ansbr

--- lesson-3/ansible/util/add-bridges.sh
+++ lesson-3/ansible/util/add-bridges.sh
@@ -5,10 +5,7 @@ sudo brctl addbr ans-br2
 sudo brctl addbr frr-2-ext
 sudo brctl addbr frr-3-ext
 
-# sudo ip addr add 10.10.92.2/24 dev frr-2-host
-# sudo ip addr add 10.10.93.2/24 dev frr-3-host
-
-sudo ifconfig frr-1-host up
-sudo ifconfig frr-2-host up
+sudo ifconfig ans-br1 up
+sudo ifconfig ans-br2 up
 sudo ifconfig frr-2-external up
 sudo ifconfig frr-3-external up
